560c05f8c98030dc512b421b539623a2a1a4a357
Harrison Unruh <harrison.unruh@gmail.com>
2017-03-27 23:23:05 -0400

    Added raw expo splash, minor tweaks to applications page

---
+++ js/rawexpo.js
@@ -0,0 +1,40 @@
+// Code courtesy of CodePen: https://codepen.io/SitePoint/pen/MwNPVq
+function getTimeRemaining(endtime) {
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor((t / 1000) % 60);
+	var minutes = Math.floor((t / 1000 / 60) % 60);
+	var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+}
+
+function initializeClock(endtime) {
+	var daysSpan = document.getElementById('days');
+	var hoursSpan = document.getElementById('hours');
+	var minutesSpan = document.getElementById('minutes');
+	var secondsSpan = document.getElementById('seconds');
+
+	function updateClock() {
+		var t = getTimeRemaining(endtime);
+
+		daysSpan.innerHTML = t.days;
+		hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+		minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+		secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+		if (t.total <= 0) {
+			clearInterval(timeinterval);
+		}
+	}
+
+	updateClock();
+	var timeinterval = setInterval(updateClock, 1000);
+}
+
+initializeClock(new Date("April 27, 2017 17:00:00"));
